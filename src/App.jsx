4a939b75c18ec1c16c700e42261bb04c6c52386f
Nindya Putri Maharani <nindyaputrimr@gmail.com>
2023-06-14 13:30:59 +0700

    update : dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
-import CardDashboard from "./elements/Card/CardDashboard";
 import Dashboard from "./pages/Dashboard/DashboardPages";
 
 function App() {
